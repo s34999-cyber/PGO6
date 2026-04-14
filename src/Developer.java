26c5312454a4901fbf5ef1657ca8c2e050bf02c2
Vincent Radecki <s34999@pjwstk.edu.pl>
2026-04-14 11:21:40 +0200

    Tasks 2&3

--- src/Developer.java
+++ src/Developer.java
@@ -6,4 +6,18 @@ public class Developer extends Employee {
         this.mainLanguage = mainLanguage;
         this.repositoryCount = repositoryCount;
     }
+
+    @Override
+    public double calculateMonthlyCost() {
+        return super.calculateMonthlyCost();
+    }
+
+    @Override
+    public String introduceYourself() {
+        return "Hello! I'm " + getFirstName() + ", a "+ mainLanguage+" developer!";
+    }
+
+    public void printTechnologies(){
+        System.out.println("I work mainly with " + mainLanguage + ", and have made " + repositoryCount + "Repositories so far.");
+    }
 }
